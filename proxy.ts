a17ce3a5ac1d4660be8e2cfde4b2bc78d845755e
arian2004 <arian2004feni@gmail.com>
2026-09-09 11:26:55 +0600

    authenticating protected routes in proxy

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,8 @@ import type { NextRequest } from "next/server";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 const AUTH_ROUTES = ["/login", "/register"];
+// const PUBLIC_ROUTES = ["/", "/news", "/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/news"];
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
@@ -32,6 +34,18 @@ export async function proxy(request: NextRequest) {
     } else {
       return NextResponse.redirect(new URL("/", request.url));
     }
+  }
+
+  // const isPublicRoute = PUBLIC_ROUTES.includes(pathname) || pathname.startsWith("/news/");
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  if(!accessToken && !isPublicRoute && !isAuthRoute){
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   // return NextResponse.redirect(new URL("/", request.url));
