98cb34430242ccb3b1a263e344e0addd1dd041d9
arian2004 <arian2004feni@gmail.com>
2026-09-09 10:55:18 +0600

    proxy file implementation, handling auth routes

---
+++ proxy.ts
@@ -0,0 +1,43 @@
+// import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "/register"];
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  // const cookieStore = await cookies();
+  // const accessToken = cookieStore.get("accessToken")?.value;
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
+
+  let userRole = null;
+
+  if (decodedToken) {
+    userRole = decodedToken.role;
+  }
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  // return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)"],
+};
