b04d199f177165987474f397f0ec5a2d448ee00f
arian2004 <arian2004feni@gmail.com>
2026-09-09 12:34:01 +0600

    Authorizing Role Based Protected Routes In Proxy

--- proxy.ts
+++ proxy.ts
@@ -2,6 +2,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { jwtUtils } from "./utils/jwt";
 
 const AUTH_ROUTES = ["/login", "/register"];
 // const PUBLIC_ROUTES = ["/", "/news", "/login", "/register"];
@@ -16,14 +17,21 @@ export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
 
   const decodedToken = accessToken
-    ? (jwt.decode(accessToken) as JwtPayload)
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
     : null;
 
   let userRole = null;
 
-  if (decodedToken) {
-    userRole = decodedToken.role;
+  if (!decodedToken?.success) {
+    // accessToken expired, clear the cookie
+    request.cookies.delete("accessToken");
+    return NextResponse.redirect(new URL("/login", request.url));
   }
+
+  if (decodedToken?.success && decodedToken.data) {
+    userRole = (decodedToken.data as JwtPayload).role;
+  }
+
   if (accessToken && AUTH_ROUTES.includes(pathname)) {
     if (userRole === "USER") {
       return NextResponse.redirect(new URL("/dashboard", request.url));
@@ -44,10 +52,20 @@ export async function proxy(request: NextRequest) {
     (route) => pathname === route || pathname.startsWith(route + "/"),
   );
 
-  if(!accessToken && !isPublicRoute && !isAuthRoute){
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
+  if (pathname.startsWith("/dashboard") && userRole !== "USER") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (
+    pathname.startsWith("/author-dashboard") &&
+    userRole !== "AUTHOR"
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (pathname.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
   // return NextResponse.redirect(new URL("/", request.url));
   return NextResponse.next();
 }
