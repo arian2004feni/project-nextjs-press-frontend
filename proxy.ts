788cc6a4b99aef9f3379d42f54dc66fe673fc783
arian2004 <arian2004feni@gmail.com>
2026-09-10 00:17:55 +0600

    Getting New Access Token Using Refresh Token In Proxy File

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,9 @@
-// import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
 import { jwtUtils } from "./utils/jwt";
+import { getNewAccessToken } from "./services/refreshToken";
 
 const AUTH_ROUTES = ["/login", "/register"];
 // const PUBLIC_ROUTES = ["/", "/news", "/login", "/register"];
@@ -12,24 +13,50 @@ const PUBLIC_ROUTES = ["/", "/news"];
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  // const cookieStore = await cookies();
+  const cookieStore = await cookies();
   // const accessToken = cookieStore.get("accessToken")?.value;
-  const accessToken = request.cookies.get("accessToken")?.value;
+  let accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
 
-  const decodedToken = accessToken
+  let decodedAccessToken = accessToken
     ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
     : null;
 
-  let userRole = null;
+  const decodedRefreshToken = refreshToken
+    ? jwtUtils.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+    : null;
 
-  if (!decodedToken?.success) {
-    // accessToken expired, clear the cookie
-    request.cookies.delete("accessToken");
-    return NextResponse.redirect(new URL("/login", request.url));
+    if (!decodedAccessToken?.success && decodedRefreshToken?.success) {
+    const result = await getNewAccessToken();
+    
+    if (result.success) {
+      const newAccessToken = result.data.accessToken;
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+      accessToken = newAccessToken;
+      decodedAccessToken = jwtUtils.verifyToken(
+        newAccessToken,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
   }
 
-  if (decodedToken?.success && decodedToken.data) {
-    userRole = (decodedToken.data as JwtPayload).role;
+  let userRole = null;
+
+  if (!decodedAccessToken?.success) {
+    // accessToken expired, clear the cookie
+    cookieStore.delete("accessToken");
+    // return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (decodedAccessToken?.success && decodedAccessToken.data) {
+    userRole = (decodedAccessToken.data as JwtPayload).role;
   }
 
   if (accessToken && AUTH_ROUTES.includes(pathname)) {
