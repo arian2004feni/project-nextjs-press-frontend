0089607c80e6a4b360679c965e0433e886092f2c
arian2004 <arian2004feni@gmail.com>
2026-09-10 19:58:53 +0600

    fix: news public post data must be public

--- app/(publicGroup)/_actions/getPublicPosts.ts
+++ app/(publicGroup)/_actions/getPublicPosts.ts
@@ -1,21 +1,7 @@
 "use server";
 
-import { cookies } from "next/headers";
-
 export default async function getPublicPosts() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-
-  if (!accessToken) {
-    return {
-      success: false,
-      message: "user not logged in",
-    };
-  }
   const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts`, {
-    headers: {
-      Cookie: `accessToken=${accessToken}`,
-    },
     cache: "force-cache",
     next: {
       revalidate: 60 * 60, // Revalidate every hour
